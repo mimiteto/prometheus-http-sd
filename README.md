4d3282f57b7566e7ae1bf1e521acf27032f5f8db
Dimitar Ivanov <mimiteto@gmail.com>
2024-05-04 00:17:08 +0300

    Adding ip network discovery

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# python-repo-template
+# prom-http-sd
+
+This util provides basic prometheus http service discovery.
+
+Note: It requires root privileges. Prefer using the provided docker image:
+
+```bash
+docker run -ti --rm prom-http-sd
+```
--- prom_http_sd/models/targets.py
+++ prom_http_sd/models/targets.py
@@ -5,10 +5,14 @@ This module defines the data models for the Prometheus HTTP Service Discovery.
 """
 
 import re
-from typing import Annotated
+from typing import Annotated, Callable, TypeVar
+from concurrent.futures import ThreadPoolExecutor, Future, as_completed
+
 from pydantic import BaseModel, PositiveInt
 from pydantic.functional_validators import AfterValidator
 from pydantic.networks import IPvAnyAddress
+
+T = TypeVar("T")
 
 
 def validate_port(port: PositiveInt) -> PositiveInt:
@@ -60,3 +64,32 @@ class Targets(BaseModel):
 class TargetsList(BaseModel):
     """ List of Targets """
     targets: list[Targets]
+
+
+def target_list_factory(
+    generator_fn: Callable[..., list[T]],
+    check_fns: list[Callable[[T], bool]],
+    producer_fn: Callable[[T], Target],
+    *args,
+    **kwargs
+) -> list[Target]:
+    """
+    Function generates targets based on provided generator function.
+    All additional args are passed to the generator function.
+    """
+    targets: list[Target] = []
+    futures: dict[Future, T] = {}
+
+    def validate(item) -> bool:
+        return all(check_fn(item) for check_fn in check_fns)
+
+    generated_items = generator_fn(*args, **kwargs)
+    with ThreadPoolExecutor(max_workers=len(generated_items)) as executor:
+        for item in generated_items:
+            futures[executor.submit(validate, item)] = item
+        for future in as_completed(futures):
+            if future.result():
+                targets.append(
+                    producer_fn(futures[future])
+                )
+    return targets
